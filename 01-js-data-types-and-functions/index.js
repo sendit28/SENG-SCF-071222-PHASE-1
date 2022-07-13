14305b26b627e9d6dd142ea41f282102f9338694
Eriberto <eriberto.guzman@outlook.com>
2022-07-12 23:10:37 -0400

    After Lecture commit

--- 01-js-data-types-and-functions/index.js
+++ 01-js-data-types-and-functions/index.js
@@ -1,9 +1,53 @@
-// Debugging
-console.log("LINKED JS")
+// // Debugging
+// console.log("LINKED JS")
+
+// let eri = "eri"
+// // JavaScript Types
+
+// // Variables
+
+// // Conditional Statement
+
+// // STRING INTERPOLATION
+// // let x = 6
+// // console.log(`1+${x}=${1+x}`)
+
+// // Number
 
 
-// JavaScript Types
+// // Integers -> 1
+// // floats -> 1.1
+// let eri = "eri"
 
-// Variables
+// debugger
+// vnoaisndaios
 
-// Conditional Statements
+
+// What a Variable
+
+// algebra -> solving for x?
+
+// expression mx + b
+
+// INTERVIEW QUESTION WHAT IS THE DIFF BETWEEN VAR LET AND CONST!
+// var
+
+
+// sort of DEPRECIATED! var
+
+// console.log(x)
+// var x = 5;
+
+// WE GO BACK!!! I PROMISE
+// const x = {};
+// console.log(x);
+// x['eri'] = "eri"
+// console.log(x) 
+
+
+let x = 5;
+console.log(x)
+x = 1+6;
+console.log(x)
+
+
