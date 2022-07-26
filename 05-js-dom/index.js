900841b995620b4df3c4901c8dc04c90eea2f0f2
Eriberto Guzman <eri.guzman@flatironschool.com>
2022-07-26 16:34:43 -0400

    Added form to html along with code in js for review

--- 05-js-dom/index.js
+++ 05-js-dom/index.js
@@ -88,39 +88,39 @@ const removeExample = () => document.querySelector(".poke__card").remove()
 removeExample();
 
 // GOES THROUGH EACH POKEMON IN POKEMONS ARRAY AND RENDERS A POKECARD
-pokemons.forEach((pokemon) => renderAPokeCard(pokemon))
+// pokemons.forEach((pokemon) => renderAPokeCard(pokemon))
 
 
-function renderAPokeCard(pokemonObj){
+// function renderAPokeCard(pokemonObj){
 
-  // Creating and Selecting necessary Elements
-  const li = document.createElement('li');
-  const h3 = document.createElement('h3');
-  const img = document.createElement('img');
-  const p = document.createElement('p');
-  const ul = document.querySelector('.poke__list')
+//   // Creating and Selecting necessary Elements
+//   const li = document.createElement('li');
+//   const h3 = document.createElement('h3');
+//   const img = document.createElement('img');
+//   const p = document.createElement('p');
+//   const ul = document.querySelector('.poke__list')
   
-  // Styling Elements
-  li.classList.add("poke__card");
-  h3.classList.add("poke__header")
-  img.classList.add("poke__img", "bounce")
-  p.classList.add("poke__description")
-  ul.style.flexDirection = "row"
-  ul.style.flexWrap = "wrap"
+//   // Styling Elements
+//   li.classList.add("poke__card");
+//   h3.classList.add("poke__header")
+//   img.classList.add("poke__img", "bounce")
+//   p.classList.add("poke__description")
+//   ul.style.flexDirection = "row"
+//   ul.style.flexWrap = "wrap"
   
-  // Modifying Elements
-  h3.innerText = pokemonObj.name
+//   // Modifying Elements
+//   h3.innerText = pokemonObj.name
 
-  img.src = pokemonObj.imgUrl
-  img.alt = pokemonObj.name
-  img.width = 150
+//   img.src = pokemonObj.imgUrl
+//   img.alt = pokemonObj.name
+//   img.width = 150
 
-  p.innerText = pokemonObj.weight
+//   p.innerText = pokemonObj.weight
 
-  // Appending Elements
-  ul.append(li);
-  li.append(h3, img, p);
-}
+//   // Appending Elements
+//   ul.append(li);
+//   li.append(h3, img, p);
+// }
 
 
 
